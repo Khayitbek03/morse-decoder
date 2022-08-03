5353a4852f882f76ca22112a8550e2a911ddce06
Khayitbek03 <xayitbekmirsoatov@gmail.com>
2022-08-03 23:48:49 +0500

    init:add js code

--- src/index.js
+++ src/index.js
@@ -39,6 +39,32 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const letterLength=10;
+    const symbolLength=2;
+    let str='';
+    for(let i=0;i<expr.length;i+=letterLength){
+        let letter=expr.slice(i,i+letterLength);
+        let morseLetter='';
+        if(letter==='**********'){
+            str+=' ';
+        }
+    }
+    for(let j=0;j<letter.length;j+=symbolLength){
+        let symbol=letter.slice(j,j+symbolLength);
+        if(symbol==='10'){
+            morseLetter+='.';
+        }
+        if(symbol==='11'){
+            morseLetter+='-';
+        }
+
+        for(let key in MORSE_TABLE){
+            if(key===morseLetter){
+                str+=MORSE_TABLE[key];
+            }
+        }
+    }
+    return str;
 }
 
 module.exports = {
